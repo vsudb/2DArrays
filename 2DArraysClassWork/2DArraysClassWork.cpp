e63777acfda5cd14b11f9a2f245badb66c991f6b
fvvcpp <vsudb2025@mail.ru>
2026-04-21 01:43:18 +0300

    add 4/2 lab

--- 2DArraysClassWork/2DArraysClassWork.cpp
+++ 2DArraysClassWork/2DArraysClassWork.cpp
@@ -1,186 +1,72 @@
 #include <stdio.h>
-const int ROW = 5;
-const int COL = 5;
+#include <stdlib.h>
 
-void print(int m[ROW][COL], int ROW, int COL);  //стандарт C99 и выше
-int sum_of_diagonals(int m[ROW][COL], int ROW, int COL);
-void transponse(int m[ROW][COL], int ROW, int COL);
-void add_matrix(int A[ROW][COL], int B[ROW][COL], int ROW, int COL);
+/* =========================================================
+ *  ВСПОМОГАТЕЛЬНАЯ ФУНКЦИЯ: обмен двух указателей int*
+ * ========================================================= */
+void swapPtr(int** a, int** b) {
+    int* tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
 
-void print(int m[ROW][COL], int ROW, int COL) {
-    printf("----------\n");
-
+void printArray(int** arr, int ROW, int COL) {
+    printf("----------------\n");
     for (int i = 0; i < ROW; i++) {
         for (int j = 0; j < COL; j++) {
-            printf("%d ", m[i][j]);
+            printf("%2d ", *(*(arr + i) + j));
         }
         printf("\n");
     }
-
-    printf("----------\n");
+    printf("----------------\n");
 }
 
+int** createArray(int ROW, int COL) {
+     
+   
+}
 
-int sum_of_diagonals(int m[ROW][COL], int ROW, int COL) {
-    int sum = 0;
+/* Удаление jagged-массива */
+void deleteArray(int** arr, int ROW) {
     for (int i = 0; i < ROW; i++) {
-        for (int j = 0; j < ROW; j++) {
-            if (i == j || (i + j) == ROW - 1) {
-                sum += m[i][j];
-            }
-        }
+        free(*(arr + i));   /* освобождаем каждую строку */
     }
-    return sum;
+    free(arr);              /* освобождаем массив указателей */
 }
 
-void transponse(int m[ROW][COL], int ROW, int COL) {
-    int temp;
-    for (int i = 0; i < ROW; i++) {
-        for (int j = 0; j < ROW; j++) {
-            temp = m[i][j];
-            m[i][j] = m[j][i];
-            m[j][i] = temp;
-            if (i == j) {
-                break;
-            }
-        }
-    }
-
-}
-
-
-// Финальный результат сложения матриц записать в матрицу А
-void add_matrix(int A[ROW][COL], int B[ROW][COL], int ROW, int COL) {
-    for (int i = 0; i < ROW; i++) {
-        for (int j = 0; j < ROW; j++) {
-            A[i][j] = A[i][j] + B[i][j];
-        }
+/* Быстрая перестановка соседних строк (swap указателей, данные не копируются) */
+void transformArray(int** arr, int ROW) {
+    for (int i = 0; i < (ROW / 2) * 2; i += 2) {
+        swapPtr(arr + i, arr + i + 1);
     }
 }
 
-int main() {
-    //-------------------------------------------------------------
-    // 1) Подсчитайте сумму чисел главной и побочной диагонали матрицы 25 баллов
-    //-------------------------------------------------------------
-    printf("1)---SUM_OF_DIAGONALS\n");
-
-    int m[][COL] = { {1, 5, 5, 5, 1},
-                    {5, 1, 5, 1, 5},
-                    {5, 5, 9, 5, 5},
-                    {5, 1, 5, 1, 5},
-                    {1, 5, 5, 5, 1} };
+/* Дублирование каждой строки: строка i → строки 2i и 2i+1 (shallow copy) */
+int** changeArray(int** in, int ROW) {
+   
+}
 
 
-
-    print(m, ROW, COL);
-    int sum = sum_of_diagonals(m, ROW, COL);
-
-    printf("---------------------\n");
-    printf("SUM_OF_DIAGONALS = %d\n", sum);
-    printf("---------------------\n");
-
-
-    //-------------------------------------------------------------
-    // 2) Транспонирование матрицы 
-    //    Поменяйте строки и столбцы местами 
-    //    (работаем в тойже матрице, без создания новой матрицы) + 10 баллов
-    //-------------------------------------------------------------
-    printf("2)------------\n");
-    printf("--TRANSPONSE--\n");
-    //printf("--------------\n");
-
-    int t[][COL] = { {1, 1, 1, 1, 1},
-                    {2, 2, 2, 2, 2},
-                    {3, 3, 3, 3, 3},
-                    {4, 4, 4, 4, 4},
-                    {5, 5, 5, 5, 5} };
-
-    print(t, ROW, COL);
+int** createLinearArray(int ROW, int COL) {
     
-    int temp;
-    for (int i = 0; i < ROW; i++) {
-        for (int j = 0; j < ROW; j++) {
-            temp = t[i][j];
-            t[i][j] = t[j][i];
-            t[j][i] = temp;
-            if (i == j) {
-                break;
-            }
-        }
-    }
-    
-    //transponse(t, ROW, COL);
-    print(t, ROW, COL);
+}
 
-    //-------------------------------------------------------------
-    // 3) Сложение матриц  + 10 баллов
-    //-------------------------------------------------------------
-    printf("3)------------\n");
-    printf("--ADD_MATRIX--\n");
+void deleteLinearArray(int** ptrs) {
+    free(*ptrs);   
+    free(ptrs);    
+}
 
+int main(void) {
+    int ROW = 7, COL = 5;
 
-    int A[][COL] = { {5, 5, 5, 5, 5},
-                    {5, 5, 5, 5, 5},
-                    {5, 5, 5, 5, 5},
-                    {5, 5, 5, 5, 5},
-                    {5, 5, 5, 5, 5} };
+    int** arr = createArray(ROW, COL);
+    printArray(arr, ROW, COL);
 
-    int B[][COL] = { {8, 8, 8, 8, 8},
-                    {8, 8, 8, 8, 8},
-                    {8, 8, 3, 8, 8},
-                    {8, 8, 8, 8, 8},
-                    {8, 8, 8, 8, 8} };
-
-    print(A, ROW, COL);
-    print(B, ROW, COL);
-    add_matrix(A, B, ROW, COL);
-    print(A, ROW, COL);
-
-
-    //-------------------------------------------------------------
-    // 4) Умножение матриц !!!(Для отчаянных на 50 баллов)
-    //    количество столбцов первой матрицы должно совпадать с количеством строк второй.
-    //    Если матрица АA имеет размер IxK, а матрица BB KxJ
-    //    , то их произведение будет иметь размер IxJ
-    //-------------------------------------------------------------
-    printf("4)------------\n");
-    printf("--MULT_MATRIX--\n");
-
-
-    int const ROW_A = 5, COL_A = 3,
-        ROW_B = 3, COL_B = 4;
-
-    int AA[ROW_A][COL_A] = { {5, 5, 5},
-                            {5, 5, 5},
-                            {5, 5, 5},
-                            {5, 5, 5},
-                            {5, 5, 5} };
-
-    int BB[ROW_B][COL_B] = { {7, 7, 7, 7},
-                            {8, 8, 8, 8},
-                            {9, 9, 9, 9} };
-
-    int C[ROW_A][COL_B];
-
-    //------------------------------------------------------------------------
-    // AA(5x3) * BB(3x4) = C(5x4)
-    // Здесь пишим перемножение матриц AA[5][3] и BB[3][4] и записываем в C[5][4]
-    //------------------------------------------------------------------------
-
-
-
-
-    /*printf("----------\n");
-
-    for (int i = 0; i < ROW_A; i++) {
-        for (int j = 0; j < COL_B; j++) {
-            printf("%d ", C[i][j]);
-        }
-        printf("\n");
-    }
-
-    printf("----------\n");*/
-
+    int** out = changeArray(arr, ROW);
+    printArray(out, ROW * 2, COL);
+              
+    deleteArray(arr, ROW);
+    free(out);
 
     return 0;
 }
